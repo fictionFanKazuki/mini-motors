0d02181f442e1c1cd73b3e5d17e119cadbec5fa2
TwiddleMega <49271517+TwiddleMega@users.noreply.github.com>
2023-10-04 22:58:29 -0400

    Update cum_bucket.py

--- cum_bucket.py
+++ cum_bucket.py
@@ -8,9 +8,32 @@ T0 = 4700 # R
 g0 = 32.1741 #ft/s^2
 n = 0.30
 
+
 def simulate(num_grains, grain_length, grain_inner_diameter, throat_diameter):
     mass = rho*num_grains*grain_length*math.pi*(grain_inner_diameter/2)^2
     pass
+
+
+def specificimpulse(impulse, propmass, gravconst):
+    return ((impulse) / (propmass * gravconst))
+
+def thrust(massflow, exhaustvelocity, exhaustarea, exhaustpressure, ambientpressure):
+    return ((massflow * exhaustvelocity) + ((exhaustpressure - ambientpressure) * exhaustarea))
+
+def machnumber(velocity, speedsound):
+    return (velocity / speedsound)
+
+def stagnationtemperature(adiabaticconst, machnumber):
+    return (1+((adiabaticconst-1)/2)*(machnumber**2))
+
+def stagnationpressure(adiabaticconst, machnumber):
+    return ((1+((adiabaticconst-1)/2)*(machnumber**2))**(adiabaticconst/(adiabaticconst-1)))
+
+def stagnationdensity(adiabaticconst, machnumber):
+    return ((1+((adiabaticconst-1)/2)*(machnumber**2))**(1/(adiabaticconst-1)))
+
+def areamachnumberrelation(adiabaticconst, machnumber)
+    return ((1/(machnumber**2))* ((2/(adiabaticconst+1))*(1+((adiabaticconst-1)/2)*(machnumber**2)))**((adiabaticconst+1)/(adiabaticconst-1)))
 
 def mass_flow(burn_area, burn_rate):
     return rho*burn_area*burn_rate
