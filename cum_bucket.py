8964996136808797edff5263ef8294e681d81a5c
fictionFanKazuki <rudhro-needs-coffee@outlook.com>
2023-10-04 23:23:32 -0400

    fucking kill me

--- cum_bucket.py
+++ cum_bucket.py
@@ -1,4 +1,5 @@
 import math
+import scipy.integrate as integrate
 rho = 0.0018 #slugs/in^3
 a = 0.0270
 gamma = 1.2
@@ -7,10 +8,13 @@ R = 2000 #psi.ft^3.slugs^-1.R^-1
 T0 = 4700 # R
 g0 = 32.1741 #ft/s^2
 n = 0.30
+speed_of_sound = 1100 #ft/s
 
 
 def simulate(num_grains, grain_length, grain_inner_diameter, throat_diameter):
     mass = rho*num_grains*grain_length*math.pi*(grain_inner_diameter/2)^2
+
+    meop = stagnationpressure(machnumber())
     pass
 
 
@@ -23,17 +27,17 @@ def thrust(massflow, exhaustvelocity, exhaustarea, exhaustpressure, ambientpress
 def machnumber(velocity, speedsound):
     return (velocity / speedsound)
 
-def stagnationtemperature(adiabaticconst, machnumber):
-    return (1+((adiabaticconst-1)/2)*(machnumber**2))
+def stagnationtemperature(machnumber):
+    return (1+((gamma-1)/2)*(machnumber**2))
 
-def stagnationpressure(adiabaticconst, machnumber):
-    return ((1+((adiabaticconst-1)/2)*(machnumber**2))**(adiabaticconst/(adiabaticconst-1)))
+def stagnationpressure(machnumber):
+    return ((1+((gamma-1)/2)*(machnumber**2))**(gamma/(gamma-1)))
 
-def stagnationdensity(adiabaticconst, machnumber):
-    return ((1+((adiabaticconst-1)/2)*(machnumber**2))**(1/(adiabaticconst-1)))
+def stagnationdensity(machnumber):
+    return ((1+((gamma-1)/2)*(machnumber**2))**(1/(gamma-1)))
 
-def areamachnumberrelation(adiabaticconst, machnumber)
-    return ((1/(machnumber**2))* ((2/(adiabaticconst+1))*(1+((adiabaticconst-1)/2)*(machnumber**2)))**((adiabaticconst+1)/(adiabaticconst-1)))
+def areamachnumberrelation(machnumber):
+    return ((1/(machnumber**2))* ((2/(gamma+1))*(1+((gamma-1)/2)*(machnumber**2)))**((gamma+1)/(gamma-1)))
 
 def mass_flow(burn_area, burn_rate):
     return rho*burn_area*burn_rate
@@ -43,3 +47,9 @@ def burn_rate(chamber_pressure):
 
 def chamber_pressure(burn_area, throat_area):
     return ((burn_area/throat_area)*cstar*a*rho)**(1/(1-n))
+
+def impulse(thrust, time):
+    return integrate(thrust, 0, time)
+
+def rocket_equation(exit_velocity, initial_mass, empty_mass):
+    return exit_velocity*math.log(initial_mass/empty_mass)
