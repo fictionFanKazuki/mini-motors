fb92b4e546ced866fc8126c24644f9553d406184
fictionFanKazuki <rudhro-needs-coffee@outlook.com>
2023-10-04 22:37:24 -0400

    beginnings

---
+++ cum_bucket.py
@@ -0,0 +1,10 @@
+rho = 0.0018 #slugs/in^3
+a = 0.0270
+gamma = 1.2
+cstar = 4890 # ft/s
+R = 2000 #psi.ft^3.slugs^-1.R^-1
+T0 = 4700 # R
+
+
+def simulate(num_grains, grain_length, grain_inner_diameter, throat_diameter):
+    pass
