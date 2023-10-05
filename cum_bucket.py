72d458ee969761a6c6db311bee0e819e656a9e8c
fictionFanKazuki <rudhro-needs-coffee@outlook.com>
2023-10-04 22:46:55 -0400

    end my suffering

--- cum_bucket.py
+++ cum_bucket.py
@@ -1,10 +1,13 @@
+import math
 rho = 0.0018 #slugs/in^3
 a = 0.0270
 gamma = 1.2
 cstar = 4890 # ft/s
 R = 2000 #psi.ft^3.slugs^-1.R^-1
 T0 = 4700 # R
-
+g0 = 32.1741 #ft/s^2
 
 def simulate(num_grains, grain_length, grain_inner_diameter, throat_diameter):
+    mass = rho*num_grains*grain_length*math.pi*(grain_inner_diameter/2)^2
     pass
+
